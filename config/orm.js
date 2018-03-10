057d0e03f1bf0e7d4ac10c50ce8e9bd354ce33f4
Avash Chapagain <Avash@Avashs-MacBook-Pro.local>
2018-03-09 23:31:35 -0500

    first commit

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+var connection = require('./connection.js');
+
+//Object relational Mapper
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = 'SELECT * FROM ' + tableInput;
+        connection.query(queryString, function(err, result) {
+            cb(result);
+        });
+    },
+    insertOne: function(tableInput, nameInput, devourInput, cb) {
+        var queryString = 'INSERT INTO ' + tableInput + ' SET ?'
+        connection.query(queryString, {burger_name: nameInput, devoured: devourInput}, function(err, result) {
+            cb(result);
+        });
+    },
+    updateOne: function(tableInput, idInput, cb) {
+        var queryString = 'UPDATE ' + tableInput + ' SET devoured = 1 WHERE ?'
+        connection.query(queryString, [{id: idInput}], function(err, result) {
+            cb(result);
+        });
+    },
+    deleteOne: function(tableInput, idInput, cb) {
+        var queryString = 'DELETE FROM ' + tableInput + ' WHERE ?'
+        connection.query(queryString, {id: idInput}, function(err, result) {
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
